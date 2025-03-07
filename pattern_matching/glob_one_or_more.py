26341ec333afc132c2665fd7f571d05396b1086d
rijojohn85 <rijojohn85@gmail.com>
2025-03-07 14:43:11 +0530

    done with one ore more

--- pattern_matching/glob_one_or_more.py
+++ pattern_matching/glob_one_or_more.py
@@ -8,11 +8,15 @@ class OneOrMore(Match):
         self.char = char
 
     def _match(self, text: str, start: int) -> Optional[int]:
-        if text[start] != self.char:
+        # if first char is not self.char return None: fail
+        if text[start] != self.char or start >= len(text):
             return None
+        # find index after start + 1 where char is not self.char
         next_index = next(
             (i for i, char in enumerate(text[start + 1 :]) if char != self.char), -1
         )
-        if next_index == -1:
-            return self.rest._match(text, len(text))
-        return self.rest._match(text, start + next_index + 1)
+        # if next index == -1, we have reached end of string, so send len(text) else poisition of next char
+        index = len(text) if next_index == -1 else (start + next_index + 1)
+
+        # pass it on the to next check.
+        return self.rest._match(text, index)
